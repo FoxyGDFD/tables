0507b77ad1278bee5aefff2355e778b7ccb00900
FoxyGDFD <mic.panfil0v@gmail.com>
2024-01-13 11:46:20 +0300

    feat: lazy load images

--- src/features/ImageCard/ImageCard.tsx
+++ src/features/ImageCard/ImageCard.tsx
@@ -3,6 +3,6 @@ import { FC } from 'react';
 
 export const ImageCard: FC<{ img: string }> = ({ img }) => (
   <BaseCard className='w-full max-h-[216px] h-full grid place-content-center'>
-    <img src={img} className='max-h-[138px] object-cover' />
+    <img src={img} loading='lazy' className='max-h-[138px] object-cover' />
   </BaseCard>
 );
